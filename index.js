8a022d2daeb20f9629c533a785fade389c454ab8
Rob Bauman <rbauman08@gmail.com>
2022-02-03 15:51:19 -0500

    Added postrgres

--- index.js
+++ index.js
@@ -1,38 +1,80 @@
+if (process.env.NODE_ENV !== 'production') {
+    console.log("loading env")
+    require('dotenv').config();
+}
 const express = require('express')
 const app = express()
-const fs = require("fs")
 const cors = require("cors")
 const bodyParser = require("body-parser")
-const words = require("./words.js")
 
 app.use(cors({
-    origin: "http://localhost:3000"
+    origin: "*"
 }))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: process.env.dbuser,
+  host: process.env.dbhost,
+  database: process.env.dbname,
+  password: process.env.dbpw,
+  port: process.env.dbport,
+})
+
+const getDailyWord = (date) => {
+    return pool.query("SELECT * FROM daily_words WHERE date = $1", [date], (error, results) => {
+        if (error) {
+            throw error;
+        }
+        return results.rows;
+    });
+}
+
+// app.post('/insertWords', (req, res) => {
+//     words.dict.forEach(word => {
+//         pool.query("INSERT INTO words (word) VALUES ($1)", [word], (error, result) => {
+//             console.log(word + ' inserted')
+//         })
+//     })
+// })
+
 app.post('/checkWord', function (req, res) {
     console.log(req.body)
     if (req.body.guessWord) {
-        fs.readFile( __dirname + "/dailyWords.json", 'utf8', function (err, data) {
-            data = JSON.parse(data)
-
-            const guessWord = req.body.guessWord
-            const today = new Date()
-            const dateString = `${today.getDate()}${today.getMonth()}${today.getFullYear()}`
-
-            if (!data[dateString]) {
-                const index = Math.floor(Math.random() * (15918 - 0) + 0)
-                data[dateString] = words.dict[index].toUpperCase()
-                fs.writeFileSync(__dirname + "/dailyWords.json", JSON.stringify(data, null, 2))
+        const today = new Date()
+        const dateString = `${today.getDate()}${today.getMonth()}${today.getFullYear()}`
+        let correctWord = ""
+        pool.query("SELECT * FROM daily_words WHERE date = $1", [dateString], (error, results) => {
+            if (error) {
+                throw error
             }
 
-            correctWord = data[dateString]
+            if (results.rows.length === 0) {
+                pool.query("SELECT * FROM words ORDER BY random() LIMIT 1", (error, results) => {
+                    if (error) {
+                        throw error
+                    }
 
-            let results = []
+                    correctWord = results.rows[0].word.toUpperCase()
+                    console.log("Retrieved new word: " + correctWord)
+
+                    pool.query("INSERT INTO daily_words (date, word) VALUES ($1, $2)", [dateString, correctWord], (error, results) => {
+                        if (error) {
+                            throw error
+                        }
+                    })
+                })
+            } else {
+                correctWord = results.rows[0].word.toUpperCase()
+            }
+
+            const guessWord = req.body.guessWord.toUpperCase()
+
+            let guessResults = []
 
             if (guessWord === correctWord) {
-                results = [1, 1, 1, 1, 1]
+                guessResults = [1, 1, 1, 1, 1]
             } else {
                 guessWord.split("").forEach((letter, index) => {
                     let indices = []
@@ -43,39 +85,28 @@ app.post('/checkWord', function (req, res) {
                     })
 
                     if (indices.length === 0) {
-                        results.push(-1)
+                        guessResults.push(-1)
                     } else if (indices.indexOf(index) !== -1) {
-                        results.push(1)
+                        guessResults.push(1)
                     } else {
-                        results.push(0)
+                        guessResults.push(0)
                     }
-
-                    // if (correctWord.split("").indexOf(letter) === -1) {
-                    //     results.push(-1)
-                    // } else if (indices.indexOf(index) !== -1) {
-                    //     results.push(1)
-                    // } else {
-                    //     results.push(0)
-                    // }
-
-                    // if (letterIndex === -1) {
-                    //     results.push(-1)
-                    // } else if (letterIndex !== -1 && letterIndex !== index) {
-                    //     results.push(0)
-                    // } else {
-                    //     results.push(1)
-                    // }
                 });
             }
 
-            res.end(JSON.stringify(results));
+            res.end(JSON.stringify(guessResults));
         });
     } else {
         res.send("No guess word provided")
     }
 })
 
-const server = app.listen(8081, function () {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8081;
+}
+
+const server = app.listen(port, function () {
     const host = server.address().address
     const port = server.address().port
     console.log("Example app listening at http://%s:%s", host, port)
